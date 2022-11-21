f5f12ceb889e6067fb010fd4fa428ba4817a2707
azheng04 <azheng04@wm.edu>
2022-11-21 18:18:21 -0500

    Winning and Losing Screen with Navigations

--- app/src/main/java/com/example/amazebyannemariezheng/WinningActivity.java
+++ app/src/main/java/com/example/amazebyannemariezheng/WinningActivity.java
@@ -2,13 +2,34 @@ package com.example.amazebyannemariezheng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WinningActivity extends AppCompatActivity {
-
+    private Button playAgain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winning);
+
+        playAgain = (Button) findViewById(R.id.btnNewGame);
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this,AMazeActivity.class);
+        startActivity(intent);
+
+    };
+    @Override
+    public void onBackPressed(){
+        openActivity();
     }
 }
