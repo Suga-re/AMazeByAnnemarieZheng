c6c1ac966627e4843ea40c46475bbb298d703176
azheng04 <azheng04@wm.edu>
2022-11-20 21:24:26 -0500

    Hello World with Navigation Button to Title state

---
+++ app/src/main/java/com/example/amazebyannemariezheng/AMazeActivity.java
@@ -0,0 +1,14 @@
+package com.example.amazebyannemariezheng;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AMazeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amaze);
+    }
+}
--- app/src/main/java/com/example/amazebyannemariezheng/MainActivity.java
+++ app/src/main/java/com/example/amazebyannemariezheng/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.amazebyannemariezheng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView greeting;
+    private Button click;
+    private Button ToGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greeting =(TextView)findViewById(R.id.tvGreeting);
+        click =(Button) findViewById (R.id.btnClick);
+        ToGame=(Button) findViewById(R.id.btnToGame);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                greeting.setVisibility(View.VISIBLE);
+            }
+        });
+        ToGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity();
+            }
+        });
     }
+    public void openActivity(){
+        Intent intent = new Intent(this,AMazeActivity.class);
+        startActivity(intent);
+
+    };
 }
